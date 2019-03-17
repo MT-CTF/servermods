386b429fb6719d396feaaf57c56711b8a9410e55
ANAND <ClobberXD@gmail.com>
2019-03-17 09:04:46 +0530

    Add spectate priv and spectate chat-commands (#5)

--- init.lua
+++ init.lua
@@ -1,3 +1,101 @@
+local spectators = {}
+
+minetest.register_privilege("spectate", {
+	description = "Can spectate other players"
+})
+
+minetest.register_chatcommand("watch", {
+	params = "<name>",
+	description = "Spectate another player",
+	privs = {spectate = true},
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+		if not player then
+			return false, "You should be online to be able to spectate another player!"
+		end
+
+		if not param or param:trim() == "" then
+			return false, "Invalid parameters, see /help spectate"
+		end
+
+		param = param:trim()
+		local target = minetest.get_player_by_name(param)
+		if not target then
+			return false, "Player " .. param .. " isn't online!"
+		end
+
+		if player:get_attach() then
+			player:set_detach()
+		end
+
+		if not spectators[name] then
+			spectators[name] = {}
+		end
+
+		spectators[name].target = param
+		local hud_text = "Spectating " .. param .. "..."
+		local hud_id = spectators[name].hud
+		if hud_id then
+			player:hud_change(hud_id, "text", hud_text)
+		else
+			spectators[name].hud = player:hud_add({
+				hud_elem_type = "text",
+				position = {x = 0.5, y = 0.5},
+				offset = {x = 0, y = 100},
+				alignment = {x = 0, y = 0},
+				number = 0x4444CC,
+				text = hud_text
+			})
+		end
+		player:set_attach(target, "", {x = 0, y = 0, z = 0}, {x = 0, y = 0, z = 0})
+		return true, minetest.colorize("#4444CC", hud_text)
+	end
+})
+
+minetest.register_chatcommand("unwatch", {
+	params = "",
+	description = "Stop spectating another player",
+	privs = {spectate = true},
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+		if not player then
+			return false, "You should be online to run this command!"
+		end
+
+		if player:get_attach() then
+			player:hud_remove(spectators[name].hud)
+			player:set_detach()
+			return true, minetest.colorize("#4444CC", "Stopped spectating " ..
+					spectators[name].target .. "!")
+		end
+		spectators[name] = nil
+
+		return true
+	end
+})
+
+minetest.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	for sname, tname in pairs(spectators) do
+		-- Spectator left
+		if name == sname then
+			spectators[sname] = nil
+			return
+		-- Target left
+		elseif name == tname then
+			minetest.chat_send_player(sname, minetest.colorize("#4444CC",
+							"Target left. Stopped spectating " ..
+							tname .. "!"))
+			local spectator = minetest.get_player_by_name(sname)
+			if spectator and spectators[sname].hud then
+				spectator:hud_remove(spectators[sname].hud)
+			end
+			spectators[sname] = nil
+			return
+		end
+	end
+end)
+
 minetest.register_privilege("ctf_server")
 minetest.override_chatcommand("ctf_queue_restart", {
         privs = { ctf_server = true },
@@ -57,7 +155,7 @@ end)
 local old_set = playertag.set
 function playertag.set(player, type, color)
 	local privs = minetest.get_player_privs(player:get_player_name())
-	if privs.interact or not privs.fly then
+	if not privs.spectate then
 		return old_set(player, type, color)
 	end
 end
@@ -65,19 +163,10 @@ end
 local old_add_gauge = gauges.add_HP_gauge
 function gauges.add_HP_gauge(name)
 	local privs = minetest.get_player_privs(name)
-	if privs.interact or not privs.fly then
+	if not privs.spectate then
 		return old_add_gauge(name)
 	end
 end
-
---table.insert(minetest.registered_on_joinplayers, 1, function(player)
---	local name = player:get_player_name()
---	local info = minetest.get_player_information(name)
---	if info.protocol_version < 32 then
---		minetest.kick_player(name, "Please update to 0.4.16. Get newer versions from minetest.net/downloads or by searching for Minetest on the Play Store")
---		return true
---	end
---end)
 
 
 local function hide_player(player)
@@ -98,8 +187,7 @@ minetest.register_on_joinplayer(function(player)
 			return
 		end
 
-		if not minetest.check_player_privs(name, { fly = true })
-				or minetest.check_player_privs(name, { interact = true }) then
+		if not minetest.check_player_privs(name, { spectate = true }) then
 			return
 		end
 
