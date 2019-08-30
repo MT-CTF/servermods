f9fb02d88a60e7b15fc3764823df9cd3826e891f
Xenon <36995016+xenonca@users.noreply.github.com>
2019-08-30 10:56:30 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -223,20 +223,30 @@ minetest.register_chatcommand("whereis", {
 	end
 })
 
+local staff = {}
 minetest.register_chatcommand("s", {
-    params = "<msg>",
-    description = "Send a message on the staff channel",
-    privs = {kick=true},
-    func = function(name, param)
-    local staff = {}
-    for _, player in pairs(minetest.get_connected_players()) do
-        local toname = player:get_player_name()
-        if minetest.check_player_privs(toname, {kick=true}) then
-            table.insert(staff, toname)
-            minetest.chat_send_player(toname, minetest.colorize("#ff6600",
-                    "«" .. name .. "» " .. param))
-            minetest.log("action", "CHAT[STAFFCHANNEL]: <" .. name .. "> " .. param)
-        end
-    end
-end
+	params = "<msg>",
+	description = "Send a message on the staff channel",
+	privs = { ban = true },
+	func = function(name, param)
+		for _, toname in pairs(staff) do
+			minetest.chat_send_player(toname, minetest.colorize("#ff6600",
+				"«" .. name .. "» " .. param))
+			minetest.log("action", "CHAT [STAFFCHANNEL]: <" .. name .. "> " .. param)
+		end
+	end
 })
+
+minetest.register_on_joinplayer(function(player)
+	if minetest.check_player_privs(player, { kick = true, ban = true }) then
+		table.insert(staff, player:get_player_name())
+	end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	local idx = table.indexof(staff, name)
+	if idx ~= -1 then
+		table.remove(staff, idx)
+	end
+end)
