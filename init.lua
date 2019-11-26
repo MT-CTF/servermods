dcbf6308afa282580fccc93dc288a34b40ee5209
Xenon <36995016+xenonca@users.noreply.github.com>
2019-11-26 18:02:15 +0100

    Add in-game staff channel (#6)

--- init.lua
+++ init.lua
@@ -228,3 +228,31 @@ minetest.register_chatcommand("whereis", {
 				pos.x, pos.y, pos.z)
 	end
 })
+
+local staff = {}
+minetest.register_chatcommand("s", {
+	params = "<msg>",
+	description = "Send a message on the staff channel",
+	privs = { kick = true, ban = true },
+	func = function(name, param)
+		for _, toname in pairs(staff) do
+			minetest.chat_send_player(toname, minetest.colorize("#ff9900",
+				"<" .. name .. "> " .. param))
+			minetest.log("action", "CHAT [STAFFCHANNEL]: <" .. name .. "> " .. param)
+		end
+	end
+})
+
+minetest.register_on_joinplayer(function(player)
+	if minetest.check_player_privs(player, { kick = true, ban = true }) then
+		table.insert(staff, player:get_player_name())
+	end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	local idx = table.indexof(staff, name)
+	if idx ~= -1 then
+		table.remove(staff, idx)
+	end
+end)
