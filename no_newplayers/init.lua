0ac85a6165237989a552708c1a80f17e67ccc064
LoneWolfHT <lonewolf04361@gmail.com>
2020-09-24 14:51:38 -0700

    Add no_newplayers mod

---
+++ no_newplayers/init.lua
@@ -0,0 +1,34 @@
+local modstorage = minetest.get_mod_storage()
+
+local no_new_players = modstorage:get_int("no_new_players") == 1
+
+minetest.register_on_prejoinplayer(function(name, ip)
+	if no_new_players and minetest.get_auth_handler().get_auth(name) == nil then
+		return "We aren't accepting new players right now. " ..
+				"Please try again another day or contact us on the forums https://forum.minetest.net/viewtopic.php?f=10&t=13157"
+	end
+end)
+
+minetest.register_chatcommand("no_newplayers", {
+	description = "Toggle/Check wether new players are allowed or not",
+	params = "[yes/no/show]",
+	privs = {ban = true},
+	func = function(name, param)
+		if param then
+			if param:find("y") then
+				no_new_players = true
+				modstorage:set_int("no_new_players", 1)
+			elseif param:find("n") then
+				no_new_players = false
+				modstorage:set_int("no_new_players", 0)
+			elseif not param:find("s") and not param == "" then
+				return false, minetest.colorize("#ad00af", "Usage: /no_newplayers [yes/no/show]")
+			end
+		end
+
+		return true, minetest.colorize(
+			"#ad00af",
+			"New players are" .. (no_new_players and "n't allowed" or " allowed")
+		)
+	end,
+})
