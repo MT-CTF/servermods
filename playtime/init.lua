c703e9ba6d845a2d08556a4cea7a473761ff48fb
pl608 <78400949+pl608@users.noreply.github.com>
2024-05-26 17:02:59 -0700

    Updated playtime mod
    Now allows checking other players playtime if that player is online. Also added some google translate translations for some of the text I added/updated

--- playtime/init.lua
+++ playtime/init.lua
@@ -53,15 +53,24 @@ local function format_duration(seconds)
 end
 
 minetest.register_chatcommand("playtime", {
-	params = "",
+	params = "<player_name>",
 	description = S("See your total playtime on this server"),
-	func = function(name)
-		if minetest.get_player_by_name(name) then
+	func = function(name, player_name)
+		if player_name == '' then
+            player_name = name
+        end
+		if (player_name == '') and minetest.is_singleplayer() then
+			player_name = 'singleplayer'
+		end
+		if not minetest.player_exists(player_name) then
+			return false, S('You must enter a existing player name!')
+		end
+		if minetest.get_player_by_name(player_name) then
 			return true,
-				C("#63d437", "Total: ")..C("#ffea00", format_duration(playtime.get_total_playtime(name))).."\n"..
-				C("#63d437", "Current: ")..C("#ffea00", format_duration(playtime.get_session_playtime(name)))
+				C("#63d437", "Total: ")..C("#ffea00", format_duration(playtime.get_total_playtime(player_name))).."\n"..
+				C("#63d437", "Current: ")..C("#ffea00", format_duration(playtime.get_session_playtime(player_name)))
 		else
-			return false, S("You must be connected to run this command!")
+			return false, S("Player must be online to run this command!")
 		end
 	end,
 })
