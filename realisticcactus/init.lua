f6d6612fe2aae5014161bbbcb8dae1f089198736
Themostrandom <165834755+Themostrandom@users.noreply.github.com>
2024-08-20 13:08:01 +0200

    Update init.lua
    Correction of that :
        realisticcactus/init.lua:7:66: line contains trailing whitespace
        realisticcactus/init.lua:11:45: line contains trailing whitespace

--- realisticcactus/init.lua
+++ realisticcactus/init.lua
@@ -4,11 +4,11 @@ minetest.register_abm({
     interval = 1.0,
     chance = 1,
     action = function(pos, node, active_object_count, active_object_count_wider)
-        local objs = minetest.get_objects_inside_radius(pos, 1.5) 
+        local objs = minetest.get_objects_inside_radius(pos, 1.5)
 		for _, obj in ipairs(objs) do
             if obj:is_player() then
                 minetest.log("action", "[cactus_mod] Player taking damage from cactus.")
-                obj:set_hp(obj:get_hp() - 1) 
+                obj:set_hp(obj:get_hp() - 1)
             end
         end
     end,
